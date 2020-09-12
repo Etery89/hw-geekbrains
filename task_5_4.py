7269c5d21c77b0d598be5c9751274df1c66e395f
etery <shadowX110@yandex.ru>
2020-09-12 23:28:27 +0300

    completed tasks 1-5

---
+++ task_5_4.py
@@ -0,0 +1,34 @@
+"""
+4. Создать (не программно) текстовый файл со следующим содержимым:
+One — 1
+Two — 2
+Three — 3
+Four — 4
+Необходимо написать программу, открывающую файл на чтение и считывающую построчно данные.
+При этом английские числительные должны заменяться на русские.
+Новый блок строк должен записываться в новый текстовый файл.
+"""
+
+
+def get_new_line(line_file, num):
+    if num in rus_nums:
+        rus_num = rus_nums[num]
+    line_list = line.strip().split()
+    line_list.remove(line_list[0])
+    line_list.insert(0, rus_num)
+    new_line = " ".join(line_list)
+    return new_line
+
+
+rus_nums = {1: "Один", 2: "Два", 3: "Три", 4: "Четыре"}
+
+
+if __name__ == "__main__":
+    count = 1
+    with open("test4.txt", "r") as t:
+        for line in t:
+            new_line = get_new_line(line, count)
+            count += 1
+            with open("new_test4.txt", 'a') as nt:
+                nt.write(new_line)
+                nt.write("\n")
