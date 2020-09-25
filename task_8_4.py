bd8fa526f62004bee1b3d20a936808004fd8bc94
etery <shadowX110@yandex.ru>
2020-09-25 22:46:50 +0300

    finalized task 6

--- task_8_4.py
+++ task_8_4.py
@@ -9,8 +9,12 @@
 Для хранения данных о наименовании и количестве единиц оргтехники, а также других данных,
 можно использовать любую подходящую структуру, например словарь.
 хранилище = {принтеры: [], сканеры: [], ксероксы: []}
+6. Продолжить работу над вторым заданием. Реализуйте механизм валидации вводимых пользователем данных.
+Например, для указания количества принтеров, отправленных на склад, нельзя использовать строковый тип данных.
 """
-
+class ValueNotCorrect(Exception):
+    def __init__(self, msg):
+        self.message = msg
 
 class Warehouse:
     warehouse = {
@@ -25,7 +29,12 @@ class Warehouse:
     }
 
     def receiving_equipment(self, name_equip, cls_equip, num):
-        self.warehouse[name_equip].append({cls_equip.name: [cls_equip, num]})
+        try:
+            if not self.validation_equip_nums(name_equip, num) == "Данные корректны":
+                raise ValueNotCorrect("Ошибка при вводе данных")
+            self.warehouse[name_equip].append({cls_equip.name: [cls_equip, num]})
+        except ValueNotCorrect as error:
+            print(error)
 
     def transfer_to_a_division(self, num_division, type_equip, cls_equip, num):
         self.records_of_equipment[num_division][type_equip].append([cls_equip, num])
@@ -34,6 +43,15 @@ class Warehouse:
             if cls_equip.name == list(equip.keys())[0]:
                 num_equip_in_wh = equip[cls_equip.name][1]
                 equip[cls_equip.name][1] = num_equip_in_wh - num
+
+    def validation_equip_nums(self, name_equip, num):
+        types_equipment = ['printers', 'scanners', 'copiers']
+        if name_equip in types_equipment and isinstance(num, int):
+            return "Данные корректны"
+        return "Данные не корректны"
+
+
+
 
 
 class OfficeEquipment:
@@ -67,13 +85,16 @@ class Copier(OfficeEquipment):
 
 scanner_1 = Scanner("HP", 200)
 warehouse_1 = Warehouse()
-# print(scanner_1.name)
+print(warehouse_1.validation_equip_nums("hy", 9))
+print(scanner_1.name)
 warehouse_1.receiving_equipment("scanners", scanner_1, 10)
-# print(warehouse_1.warehouse)
+print(warehouse_1.warehouse)
 printer_1 = Printer("Xerox", 400, True, False)
 warehouse_1.receiving_equipment("printers", printer_1, 5)
-# print(warehouse_1.warehouse)
+print(warehouse_1.warehouse)
 warehouse_1.transfer_to_a_division(2, "scanners", scanner_1, 6)
 print(warehouse_1.warehouse)
 print(warehouse_1.records_of_equipment)
+warehouse_1.receiving_equipment("printer", printer_1, 5)
+
 
