06b9be5c5175911d01f29dab0796486c576b8387
etery <shadowX110@yandex.ru>
2020-09-25 21:02:53 +0300

    completed tasks 5 and 6

--- task_8_4.py
+++ task_8_4.py
@@ -4,18 +4,36 @@
 Эти классы — конкретные типы оргтехники (принтер, сканер, ксерокс).
 В базовом классе определить параметры, общие для приведенных типов.
 В классах-наследниках реализовать параметры, уникальные для каждого типа оргтехники.
+5. Продолжить работу над первым заданием.
+Разработать методы, отвечающие за приём оргтехники на склад и передачу в определенное подразделение компании.
+Для хранения данных о наименовании и количестве единиц оргтехники, а также других данных,
+можно использовать любую подходящую структуру, например словарь.
+хранилище = {принтеры: [], сканеры: [], ксероксы: []}
 """
 
 
 class Warehouse:
-    warehouse = {}
+    warehouse = {
+        'printers': [],
+        'scanners': [],
+        'copiers': []
+    }
+    records_of_equipment = {
+        1: {'printers': [], 'scanners': [], 'copiers': []},
+        2: {'printers': [], 'scanners': [], 'copiers': []},
+        3: {'printers': [], 'scanners': [], 'copiers': []}
+    }
 
-    def __init__(self, name, num):
-        self.name = name
-        self.num = num
+    def receiving_equipment(self, name_equip, cls_equip, num):
+        self.warehouse[name_equip].append({cls_equip.name: [cls_equip, num]})
 
-    def get_warehouse(self):
-        self.warehouse[self.name] = self.num
+    def transfer_to_a_division(self, num_division, type_equip, cls_equip, num):
+        self.records_of_equipment[num_division][type_equip].append([cls_equip, num])
+        equip_in_wh = self.warehouse[type_equip]
+        for equip in equip_in_wh:
+            if cls_equip.name == list(equip.keys())[0]:
+                num_equip_in_wh = equip[cls_equip.name][1]
+                equip[cls_equip.name][1] = num_equip_in_wh - num
 
 
 class OfficeEquipment:
@@ -45,3 +63,17 @@ class Copier(OfficeEquipment):
         super().__init__(name)
         self.type_print = type_print
         self.copy_permission = copy_permission
+
+
+scanner_1 = Scanner("HP", 200)
+warehouse_1 = Warehouse()
+# print(scanner_1.name)
+warehouse_1.receiving_equipment("scanners", scanner_1, 10)
+# print(warehouse_1.warehouse)
+printer_1 = Printer("Xerox", 400, True, False)
+warehouse_1.receiving_equipment("printers", printer_1, 5)
+# print(warehouse_1.warehouse)
+warehouse_1.transfer_to_a_division(2, "scanners", scanner_1, 6)
+print(warehouse_1.warehouse)
+print(warehouse_1.records_of_equipment)
+
