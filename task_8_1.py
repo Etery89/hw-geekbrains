fbd35901aeab38fd8ddf3a0275eef9b7de2b511f
etery <shadowX110@yandex.ru>
2020-09-26 20:53:09 +0300

    finalized task 1

--- task_8_1.py
+++ task_8_1.py
@@ -32,23 +32,24 @@ class Date:
         return new_list_date[0], new_list_date[1], new_list_date[2]
 
     @staticmethod
-    def validation_date(day, month, year):
+    def validation_date(date_str):
+        date_list = date_str.split("-")
         list_num_from_day = [elem for elem in range(31)]
         list_num_from_month = [elem for elem in range(12)]
-        if not day in list_num_from_day:
+        if not int(date_list[0]) in list_num_from_day:
             raise Exception("День задан некорректным числом")
-        if not month in list_num_from_month:
+        if not int(date_list[1]) in list_num_from_month:
             raise Exception("Месяц задан некорректным числом")
-        if not year == 2020:
-            raise Exception("Введён год, не соответствующий текущему")
+        if not len(date_list[2]) == 4:
+            raise Exception("Год задан некорректным числом")
         print("Все данные введены корректно")
 
 
-
-day1, month1, year1 = Date.get_int_data("20-09-2020")
+date_1 = "20-09-2030"
+day1, month1, year1 = Date.get_int_data(date_1)
 print(day1)
 print(month1)
 print(year1)
-Date.validation_date(day1, month1, year1)
+Date.validation_date(date_1)
 
 
